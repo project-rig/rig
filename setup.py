0b80ff59b73329de21a4538fa98f1352d6e3eeba
Andrew Mundy <andrew.mundy@ieee.org>
2015-01-27 09:26:34 +0000

    Neaten setup file.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
+import sys
 
 setup(
     name="rig",
-    version="0.1",
+    version="0.0.1-dev",
     packages=find_packages(),
+
+    # Metadata for PyPi
+    author="Andrew Mundy",
+    description="A set of libraries for mapping problems to SpiNNaker",
+    license="GPLv2",
+
+    # Requirements
+    install_requires=["numpy>1.6", "six", "enum34"],
+    tests_require=["pytest>=2.6", "mock"],
 )
