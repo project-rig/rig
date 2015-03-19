59b9b7e691a05bd9674f9b0b4bf77ac6d48c69b7
Jonathan Heathcote <mail@jhnet.co.uk>
2015-03-19 20:50:20 +0000

    Add LookBlockingMixin.
    Implementation+tests only, no integration into anything yet! Boy that was
    simple, though!

---
+++ rig/utils/look_blocking.py
@@ -0,0 +1,77 @@
+"""Mixin which makes internally-asynchronous code optionally look blocking.
+
+Rig contains objects such as MachineController and BMPController which are
+required to deal with complex and (ideally...) high-performance I/O. To
+facilitate this, they are implemented in a non-blocking style powered by the
+Trollius library (the Python 2 asyncio backport).
+
+For many applications, the interface such an implementation presents is
+needlessly complex and so it is desireable to present a blocking, synchronous
+interface for the average user. At the same time, methods in these classes may
+wish to call other methods and to do so in a non-blocking style. Further,
+certain applications may wish to implement their own non-blocking code. As a
+result, presenting a non-blocking API is also neccessary.
+
+A simple wrapper is all that is required to make an asynchronous method behave
+like a blocking function and so the primary contribution of this mixin is to
+provide a decorator which by default makes methods behave in a blocking style.
+In order to support the non-blocking style, the decorator has an optional
+keyword argument `async` which when set to True results in the native
+non-blocking behaviour being exposed.
+
+The secondary contribution of this mixin is to enforce the storage of a
+reference to the event loop in use. Further, the mixin automatically creates
+such an event loop if one is not provided further hiding any asynchronous magic
+to users who do not wish to use it.
+
+.. Warning::
+    All documentation refers to :py:mod:`asyncio` even though this
+    implementation uses Trollius for Python 2 compatibility. This is because
+    the Trollius API is identical to that of :py:mod:`asyncio` and is the
+    recommended source of documentation.
+"""
+
+import trollius
+
+import functools
+
+
+class LookBlockingMixin(object):
+    """Mixin which makes internally-asynchronous code optionally look blocking.
+    """
+
+    def __init__(self, loop=None):
+        """Store a reference to the event loop in use.
+
+        Parameters
+        ----------
+        loop : :py:class:`asyncio.BaseEventLoop` or None
+            The event loop to use. If None, :py:func:`asyncio.get_event_loop`
+            will be used to get a suitable event loop.
+        """
+        if loop is None:
+            loop = trollius.get_event_loop()
+        self.loop = loop
+
+    @staticmethod
+    def look_blocking(f):
+        """Decorator which makes a coroutine function/future-returning function
+        (optionally) block until the coroutine/future completes.
+
+        The decorator adds an additional `async` keyword argument to any
+        decorated function which defaults to False. If the argument is False,
+        the function will block as described above. If True, the
+        coroutine/future will be returned instead.
+        """
+        @functools.wraps(f)
+        def f_(*args, **kwargs):
+            self = args[0]
+
+            if kwargs.pop("async", False):
+                # Directly expose the async interface
+                return f(*args, **kwargs)
+            else:
+                # Block until the coroutine/future completes
+                return self.loop.run_until_complete(f(*args, **kwargs))
+
+        return f_
---
+++ rig/utils/tests/test_look_blocking.py
@@ -0,0 +1,78 @@
+import pytest
+
+from ..look_blocking import LookBlockingMixin
+
+import trollius
+from trollius import From, Return
+
+
+class TestException(Exception):
+    pass
+
+
+class TestClass(LookBlockingMixin):
+    """A test class with a single async method."""
+
+    def __init__(self, loop):
+        LookBlockingMixin.__init__(self, loop)
+
+    @LookBlockingMixin.look_blocking
+    @trollius.coroutine
+    def coro_sleep_and_return_arg(self, arg):
+        """Coroutine: returns its argument after a delay."""
+        yield From(trollius.sleep(0.01, loop=self.loop))
+        raise Return(arg)
+
+    @LookBlockingMixin.look_blocking
+    def fut_sleep_and_return_arg(self, arg):
+        """Returns a future which returns its value after a delay."""
+        fut = trollius.Future(loop=self.loop)
+
+        def satisfy():
+            if not fut.cancelled():  # pragma: no branch
+                fut.set_result(arg)
+        self.loop.call_later(0.01, satisfy)
+        return fut
+
+    @LookBlockingMixin.look_blocking
+    @trollius.coroutine
+    def coro_sleep_and_raise(self):
+        """Coroutine: raise an TestException exception after a delay."""
+        yield From(trollius.sleep(0.01, loop=self.loop))
+        raise TestException("I woke up on the wrong side of the bed!")
+
+    @LookBlockingMixin.look_blocking
+    @trollius.coroutine
+    def fut_sleep_and_raise(self):
+        """Return a future which raises a TestException exception after a delay
+        """
+        fut = trollius.Future(loop=self.loop)
+
+        def fail():
+            if not fut.cancelled():  # pragma: no branch
+                fut.set_exception(TestException("I don't want to wake up!"))
+        self.loop.call_later(0.01, fail)
+        return fut
+
+
+@pytest.mark.parametrize("supplied_loop", [None, trollius.new_event_loop()])
+def test_look_blocking(supplied_loop):
+    t = TestClass(supplied_loop)
+
+    # Make sure the blocking interfaces work
+    assert t.coro_sleep_and_return_arg(123) == 123
+    assert t.fut_sleep_and_return_arg(456) == 456
+    with pytest.raises(TestException):
+        t.coro_sleep_and_raise()
+    with pytest.raises(TestException):
+        t.fut_sleep_and_raise()
+
+    # Make sure the non-blocking interfaces work
+    assert t.loop.run_until_complete(
+        t.coro_sleep_and_return_arg(123, async=True)) == 123
+    assert t.loop.run_until_complete(
+        t.fut_sleep_and_return_arg(456, async=True)) == 456
+    with pytest.raises(TestException):
+        t.loop.run_until_complete(t.coro_sleep_and_raise(async=True))
+    with pytest.raises(TestException):
+        t.loop.run_until_complete(t.fut_sleep_and_raise(async=True))
